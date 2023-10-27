53db16d8e6818b6fca7471849af86d06eb503cdc
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2023-10-28 02:02:31 +0300

    initial commit

---
+++ src/main/java/com/clouify/cicdintro/CiCdIntroApplication.java
@@ -0,0 +1,13 @@
+package com.clouify.cicdintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clouify/cicdintro/CiCdIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouify.cicdintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
